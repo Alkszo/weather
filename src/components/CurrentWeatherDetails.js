033d4860514d6b0c7754e34e8e353c2a9d16b064
LateAlex <aleksander.szostakowski@gmail.com>
2020-11-30 16:23:51 +0100

    alternative details dispaly

--- src/components/CurrentWeatherDetails.js
+++ src/components/CurrentWeatherDetails.js
@@ -21,16 +21,15 @@ const CurrentWeatherDetails = ({ todaysWeather, icons, getIcon }) => {
                     <img src={getIcon(hour).length === 1  ? getIcon(hour)[0].icon : `http://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`} alt="icon" />
                     <p>{hour.weather[0].description}</p>
                 </div>
-                <div><p>Temperature: </p><br/>
-                     <p>  {(hour.main.temp * 2).toFixed() / 2} </p>                  
+                <div className="weather-numbers">
+                    <div><p>Temperature: {(hour.main.temp * 2).toFixed() / 2} </p>                  
+                    </div>
+                    <div><p>Feels like: {(hour.main.feels_like * 2).toFixed() / 2}</p>
+                    </div>
+                    <div><p>Humidity: {hour.main.humidity} %</p>
+                    </div>
                 </div>
-                <div><p>Feels like: </p><br />
-                     <p>{(hour.main.feels_like * 2).toFixed() / 2}</p>
-                </div>
-                <div><p>Humidity: </p><br />
-                     <p>{hour.main.humidity} %</p>
-                </div>
-    
+        
             </div>
         </div>     
         
